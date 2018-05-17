970b21275e0866669e1f392d827bc3d12dde5de1
FELIPE NASCIMENTO DOS SANTOS <17170084@CFP127ALUNO.LOCAL>
2018-05-17 12:06:05 -0300

    primeiro commit - cms

---
+++ CMS/login.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style_index.css">
+        <title>Login cms </title>
+    </head>
+    <body>
+        <div id="main">
+            <header>
+                <div id="imagem_logo">
+                  <img src="imagens/logo-senai.jpeg">
+                </div>
+            </header>
+            <div id="content">
+                <div id="inputs_login">
+                  <div id="titulo">
+                      Login
+                  </div>
+                  <div class="inputs">
+                      Login<input type="text" name="" value="">
+                  </div>
+
+
+
+                </div>
+
+            </div>
+            <footer>
+
+            </footer>
+
+        </div>
+
+
+    </body>
+
+
+</html>
